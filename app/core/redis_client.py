0df625fd379bca773ef0ab0c8d569116623cf107
abhayshukla-gamma <abhay.shukla@gammaedge.io>
2026-03-03 19:25:12 +0530

    cd

--- app/core/redis_client.py
+++ app/core/redis_client.py
@@ -1,7 +1,11 @@
 import redis
+import os
 
-redis_client = redis.Redis(  # redis_client redis server se connect ho raha hai
-    host="localhost",  # redis server kaha chal raha hai
-    port=6379,  # default redis port
-    decode_responses=True,  # will give data in string format not in bytes
-)
+
+redis_client= redis.from_url(os.getenv("REDIS_URL","redis://localhost:6379"))
+
+# redis_client = redis.Redis(  # redis_client redis server se connect ho raha hai
+#     host="localhost",  # redis server kaha chal raha hai
+#     port=6379,  # default redis port
+#     decode_responses=True,  # will give data in string format not in bytes
+# )
